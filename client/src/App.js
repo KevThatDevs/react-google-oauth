edefb9b9f770da091516cfb3ca0aea2b34ff04d6
klxu03 <beaver2025@gmail.com>
2022-06-06 20:22:49 -0400

    Blog Post 1 code done with client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+// Components
+import Profile from "./components/Profile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const clientId = process.env.REACT_APP_GCP_OAUTH_CLIENT_ID;
+
+	return (
+		<>
+			<GoogleOAuthProvider clientId={clientId}>
+				<div className="App">
+					<h2>I love coding</h2>
+				</div>
+				<Profile />
+			</GoogleOAuthProvider>
+		</>
+	);
 }
 
 export default App;
